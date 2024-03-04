9d2002391d1dc221adcd72400598bbfe85f7f3ec
Motunrayo <ajirotutu4real@gmail.com>
2024-03-04 20:26:15 +0100

    Add ES6 Promise implementation

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,11 @@
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    // eslint-disable-next-line no-constant-condition
+    if (true) {
+      resolve({
+        firstName,
+        lastName,
+      });
+    }
+  });
+}
