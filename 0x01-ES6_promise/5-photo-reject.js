06196d05f1025c1ca0edab581fbbf9ffa51b186a
Motunrayo <ajirotutu4real@gmail.com>
2024-03-04 22:18:36 +0100

    Added task 6, 7 8, & 9

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,8 @@
+export default function uploadPhoto(filename) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-constant-condition
+    if (true) {
+      reject(new Error(`${filename} cannot be processed`));
+    }
+  });
+}
---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => (
+      data.map((d) => ({
+        status: d.status,
+        value: d.status === 'fulfilled' ? d.value : String(d.reason),
+      }))
+    ));
+}
