00725c0be7312f57d666d7ee5a90cd53974e1d83
Motunrayo <ajirotutu4real@gmail.com>
2024-03-05 05:40:38 +0100

    Added task 4, 5 & 6

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+import Currency from './3-currency';
+
+/* eslint-disable no-underscore-dangle */
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Class Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || conversionRate !== 'number') {
+      throw new TypeError('Amount and conversion rate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
