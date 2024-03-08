e2853aed607481d3fa9e4ff05809316f803acd85
Motunrayo <ajirotutu4real@gmail.com>
2024-03-08 11:58:57 +0100

    Added task 6-10

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,7 +3,7 @@ export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
     if (this.constructor !== Building) {
-      if (!this.evacuationWarningMessage) {
+      if (typeof this.evacuationWarningMessage != 'function') {
         throw new Error('Class extending Building must override evacuationWarningMessage');
       }
     }
