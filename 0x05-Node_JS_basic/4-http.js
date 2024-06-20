74bdb21dc64d1c3d49856cfba9afc75ed542873c
Motunrayo <ajirotutu4real@gmail.com>
2024-06-20 11:28:09 +0100

    Added task 4 & 5

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const app = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const server = app.createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+server.listen(port, hostname);
+
+module.exports = app;
---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,76 @@
+const app = require('http');
+const fs = require('fs');
+
+const file = process.argv.length > 2 ? process.argv[2] : '';
+
+/* eslint-disable no-continue */
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      return reject(new Error('Cannot load the database'));
+    }
+
+    const lines = data.split('\n');
+    const categories = {};
+    let isFirstLine = true;
+
+    for (const line of lines) {
+      if (isFirstLine) {
+        isFirstLine = false;
+        continue;
+      }
+
+      if (line.trim() === '') {
+        continue;
+      }
+
+      const student = line.split(',');
+      const category = student[3].trim();
+
+      if (!categories[category]) {
+        categories[category] = [];
+      }
+
+      categories[category].push(student[0].trim());
+    }
+
+    const report = [];
+    const totalStudents = Object.values(categories).reduce((acc, curr) => acc + curr.length, 0);
+    report.push(`Number of students: ${totalStudents}`);
+
+    for (const [category, students] of Object.entries(categories)) {
+      report.push(`Number of students in ${category}: ${students.length}. List: ${students.join(', ')}`);
+    }
+    return resolve(report.join('\n'));
+  });
+});
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const server = app.createServer((req, res) => {
+  const { url } = req;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    const initialMessage = 'This is the list of students';
+
+    countStudents(file).then((val) => {
+      res.statusCode = 200;
+      res.end(`${initialMessage}\n${val}`);
+    }).catch(() => {
+      res.statusCode = 500;
+      res.end(initialMessage);
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Page not found');
+  }
+});
+
+server.listen(port, hostname);
+
+module.exports = app;
